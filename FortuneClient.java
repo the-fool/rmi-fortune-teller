71842af69d30a8e658b9bcd42caceef5a8657d01
Thomas <sketchbang@gmail.com>
2015-11-11 17:37:12 -0600

    begin client jpanel stuff

--- FortuneClient.java
+++ FortuneClient.java
@@ -1,5 +1,29 @@
 package lab8;
 
-public class FortuneClient {
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class FortuneClient extends JFrame {
+
+	public FortuneClient() {
+		JPanel panel = new JPanel();
+		getContentPane().add(panel);
+
+		
+		JButton fortuneButton = new JButton("Get fortunate");
+		fortuneButton.setBounds(50, 60, 80, 30);
+		
+		panel.add(fortuneButton);
+		setTitle("Fortunes");
+		setSize(300, 200);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		FortuneClient fc = new FortuneClient();
+		fc.setVisible(true);
+	}
 
 }
