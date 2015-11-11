fe9819dbc171a42c5909f8080731e375ec93b7cf
Thomas <sketchbang@gmail.com>
2015-11-11 17:59:54 -0600

    jframin

--- FortuneClient.java
+++ FortuneClient.java
@@ -1,21 +1,27 @@
 package lab8;
 
-import javax.swing.Box;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class FortuneClient extends JFrame {
 
 	public FortuneClient() {
-		JPanel panel = new JPanel();
+		
+		JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		getContentPane().add(panel);
 
-		
 		JButton fortuneButton = new JButton("Get fortunate");
-		fortuneButton.setBounds(50, 60, 80, 30);
-		
 		panel.add(fortuneButton);
+		
+		JLabel fortune = new JLabel();
+		fortune.setText("Fortunate are you");
+		panel.add(fortune);
+		
 		setTitle("Fortunes");
 		setSize(300, 200);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
