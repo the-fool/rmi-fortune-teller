0b4ff4abda50bde9395dfed41dca745ab59c766f
Thomas <sketchbang@gmail.com>
2015-11-11 17:14:42 -0600

    stub out fortuneserver

--- FortuneServerImpl.java
+++ FortuneServerImpl.java
@@ -1,5 +1,23 @@
 package lab8;
 
-public class FortuneServerImpl {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Random;
 
+public class FortuneServerImpl extends UnicastRemoteObject implements FortuneServerIntf {
+	Random r;
+	private static final String [] FORTUNES = {};
+	
+	protected FortuneServerImpl() throws RemoteException {
+		super();
+		r = new Random();
+	}
+
+	@Override
+	public String tellFortune() throws RemoteException {
+		
+		return null;
+	}
+	
+	
 }
