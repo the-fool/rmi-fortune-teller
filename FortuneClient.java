2e35a430aa6fd97745e42da8c5a75391436b9e1b
Thomas <sketchbang@gmail.com>
2015-11-11 18:09:42 -0600

    add button listener

--- FortuneClient.java
+++ FortuneClient.java
@@ -1,7 +1,10 @@
 package lab8;
 
-import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,9 +12,24 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class FortuneClient extends JFrame {
-
+	private FortuneServerIntf fs = null;
 	public FortuneClient() {
+		try {
+			String serverObjectName = "//127.0.0.1/Server";
+			System.err.println("Getting Connected to the server: "+ serverObjectName );
+			fs = (FortuneServerIntf) Naming.lookup(serverObjectName);
+			System.err.println("Connected to server.");
+		} catch (java.rmi.ConnectException ce) {
+			System.err.println("Connection to server failed. " + "Server may be temporarily unavailable.");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 		
+		initUI();
+		
+	}
+	private void initUI() {
 		JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		getContentPane().add(panel);
 
@@ -19,8 +37,18 @@ public class FortuneClient extends JFrame {
 		panel.add(fortuneButton);
 		
 		JLabel fortune = new JLabel();
-		fortune.setText("Fortunate are you");
+		fortune.setText("Click and foresee..");
 		panel.add(fortune);
+		
+		fortuneButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				try{
+					fortune.setText(fs.tellFortune());
+				} catch (RemoteException re) {
+					System.out.println("Error retrieving server response: " + re);
+				}
+			}
+		});
 		
 		setTitle("Fortunes");
 		setSize(300, 200);
